0af603bef200f5a248b70c3a12ad62908727161a
Blaser993 <corradodepinto.design@gmail.com>
2023-09-01 23:09:10 +0200

    refact

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// PRENDO GLI ELEMNTI CON CUI VOGLIO INTERAGIRE DAL DOM
 const numeroEl = document.getElementById("numero");
 const incrementoEl = document.getElementById("numero_incremento");
 const bottonePlus = document.getElementById("plus");
@@ -5,17 +6,21 @@ const bottoneMinus = document.getElementById("minus");
 const bottoneReset = document.getElementById("reset");
 const bottoneIncr = document.getElementById("incrementa"); 
 
-
+// VARIABILI ---------------------------
 let numero = 0;
 let numero_incremento = 1;
 let limite = 999999999;
 let pokeballIndex = 0;
 
+// GESTIONE IMMAGINI POKEBALL ------------------------
 console.log(pokeballIndex, bottoneReset.src);
 const pokeballs = [ "./img/1ball.png","./img/2ball.png","./img/3ball.png","./img/4ball.png"];
-
 bottoneReset.src = pokeballs[0];
 
+
+// FUNZIONI----------------------------------------------
+
+// Cambio immagine della pokebal
 function changePokeball() {
     bottoneReset.src = pokeballs[pokeballIndex];
     // Incrementa l'indice e riavvolge se raggiunge la fine dell'array
@@ -31,6 +36,7 @@ function aggiornaNumero() {
     numeroEl.textContent = numero;
 }
 
+// Bottone incremento
 bottonePlus.addEventListener("click", () => {
     numero = numero + numero_incremento;
     if (numero > limite){
@@ -39,6 +45,8 @@ bottonePlus.addEventListener("click", () => {
     aggiornaNumero();
 });
 
+
+// Bottone decremento
 bottoneMinus.addEventListener("click", () => {
     numero = numero - numero_incremento;
     if (numero < 0){
@@ -48,11 +56,13 @@ bottoneMinus.addEventListener("click", () => {
     
 });
 
+// Azzero counter
 bottoneReset.addEventListener("click", () => {
     numero = 0;
     aggiornaNumero();
 })
 
+// Cambio valore dell'incremento del counter ad ogni click
 bottoneIncr.addEventListener("click", () => {
     numero_incremento = numero_incremento * 10;
     if (numero_incremento > 1000){
@@ -62,9 +72,7 @@ bottoneIncr.addEventListener("click", () => {
     changePokeball();
 });
 
-
-
-
+// Richiamo funzioni per setting globali
 changePokeball();
 aggiornaIncr();
 aggiornaNumero();
