e085b6b3c8b9da761e5a573e7e8818b7ee6b1190
Blaser993 <corradodepinto.design@gmail.com>
2023-09-01 22:59:13 +0200

    cambiopokeball

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const incrementoEl = document.getElementById("numero_incremento");
 const bottonePlus = document.getElementById("plus");
 const bottoneMinus = document.getElementById("minus");
 const bottoneReset = document.getElementById("reset");
-const bottoneIncr = document.getElementById("incrementa"); // Corretto l'ID
+const bottoneIncr = document.getElementById("incrementa"); 
+
 
 let numero = 0;
 let numero_incremento = 1;
 let limite = 999999999;
+let pokeballIndex = 0;
+
+console.log(pokeballIndex, bottoneReset.src);
+const pokeballs = [ "./img/1ball.png","./img/2ball.png","./img/3ball.png","./img/4ball.png"];
+
+bottoneReset.src = pokeballs[0];
+
+function changePokeball() {
+    bottoneReset.src = pokeballs[pokeballIndex];
+    // Incrementa l'indice e riavvolge se raggiunge la fine dell'array
+    pokeballIndex = (pokeballIndex + 1) % pokeballs.length;
+    console.log(pokeballIndex,bottoneReset.src);
+  }
 
 function aggiornaIncr(){
     incrementoEl.textContent = numero_incremento; 
@@ -45,7 +59,12 @@ bottoneIncr.addEventListener("click", () => {
         numero_incremento = 1;
     }  
     aggiornaIncr();
+    changePokeball();
 });
 
+
+
+
+changePokeball();
 aggiornaIncr();
 aggiornaNumero();
