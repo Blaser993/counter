c2bbb4b2562b6be73d5b2e1454b78d2341d355bd
Blaser993 <corradodepinto.design@gmail.com>
2023-09-01 21:13:54 +0200

    aggiunto logica incremento

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 const numeroEl = document.getElementById("numero");
+const incrementoEl = document.getElementById("numero_incremento");
 const bottonePlus = document.getElementById("plus");
 const bottoneMinus = document.getElementById("minus");
 const bottoneReset = document.getElementById("reset");
+const bottoneIncr = document.getElementById("incrementa"); // Corretto l'ID
 
 let numero = 0;
+let numero_incremento = 1;
 
+function aggiornaIncr(){
+    incrementoEl.textContent = numero_incremento; 
+}
 
 function aggiornaNumero() {
     numeroEl.textContent = numero;
 }
 
-
 bottonePlus.addEventListener("click", () => {
-    numero++;
+    numero = numero + numero_incremento;
     aggiornaNumero();
 });
 
 bottoneMinus.addEventListener("click", () => {
-    if (numero > 0) {
-        numero--;
-        aggiornaNumero();
+    numero = numero - numero_incremento;
+    if (numero < 0){
+        numero = 0;
     }
+    aggiornaNumero();
+    
 });
 
 bottoneReset.addEventListener("click", () => {
@@ -28,5 +35,13 @@ bottoneReset.addEventListener("click", () => {
     aggiornaNumero();
 })
 
+bottoneIncr.addEventListener("click", () => {
+    numero_incremento = numero_incremento * 10;
+    if (numero_incremento > 1000){
+        numero_incremento = 1;
+    }  
+    aggiornaIncr();
+});
 
+aggiornaIncr();
 aggiornaNumero();
